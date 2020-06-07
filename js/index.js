4dbd688a7c530819ffe11bd0f12ff42cd296eb97
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-06-07 18:54:41 -0400

    1 mouse enter and leave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+//mouse enter and mouse leave
+
+const roadMap = document.querySelectorAll('.img-content');
+
+roadMap.forEach((element) => {
+    element.addEventListener("mouseenter", function() {
+          
+        element.style.transform = "scale(1.6)";
+        element.style.transition = "transform 2s";
+    })
+    element.addEventListener("mouseleave", function() {
+        element.style.transform = "scale(1)";
+    })
+});
