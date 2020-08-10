8a9c742fdb0956d6dde81f7d65b9c3d865f6583e
unknown <jackcharp69@gmail.com>
2020-08-09 20:33:02 -0400

    forgot to save. 4 done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+const btn = document.querySelectorAll('.btn');
+btn.forEach((e) => {
+    e.addEventListener('mouseenter', () => {
+        e.style.transform = "scale(2)";
+        e.style.transition = "2s";
+
+    })
+})
+
+btn.forEach((e) => {
+    e.addEventListener('mouseleave',() => {
+        e.style.transform = 'scale(1)';
+
+    })
+})
+
+const body= document.querySelector("body");
+body.addEventListener('keydown', () => {
+   body.style.transform = "rotate(180deg)";
+})
+
+body.addEventListener('keyup', () => {
+    body.style.transform = 'rotate(0deg)';
+})
