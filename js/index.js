0caf7ab5160434622e2315072b828e790cd69636
unknown <jackcharp69@gmail.com>
2020-08-09 20:57:18 -0400

    seven out of ten and added comments to code

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 // Your code goes here
+
+//creating a selector for button and making it larger when mouse enters
+
 const btn = document.querySelectorAll('.btn');
 btn.forEach((e) => {
     e.addEventListener('mouseenter', () => {
@@ -8,6 +11,8 @@ btn.forEach((e) => {
     })
 })
 
+//makeing the button go back to original size when mouse leaves
+
 btn.forEach((e) => {
     e.addEventListener('mouseleave',() => {
         e.style.transform = 'scale(1)';
@@ -15,14 +20,20 @@ btn.forEach((e) => {
     })
 })
 
+//creating a selector for the body and making it rotate 180 degrees on key press
+
 const body= document.querySelector("body");
 body.addEventListener('keydown', () => {
    body.style.transform = "rotate(180deg)";
 })
 
+//returning body to original orentations when key is let go
+
 body.addEventListener('keyup', () => {
     body.style.transform = 'rotate(0deg)';
 })
+
+//creating a selector for nav-container and adding a text box at end
 
 const navContainer = document.querySelector('.nav-container');
 let box = document.createElement('input');
@@ -30,10 +41,20 @@ box.type = "text";
 box.placeholder ="Click Me";
 navContainer.appendChild(box);
 
+//adding event listener for when box is focused on it turns blue
+
 box.addEventListener('focus', () => {
     box.style.background = 'blue';
 })
 
+//removing the blue background when box is blured
+
 box.addEventListener('blur', () => {
     box.style.background = '';
+})
+
+//adding an alert for when someone tries to copy anything
+
+const source = window.addEventListener('copy', (event) => {
+    alert("COPY IS NOT ALLOWED ON THIS WEBSITE");
 })
